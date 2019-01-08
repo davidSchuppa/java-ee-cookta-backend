8cea2f74d5516c49b5152cf01fafc933f1bc8c65
David Szentesi <szntsd@gmail.com>
2019-01-08 10:46:54 +0100

    init

---
+++ src/main/java/com/codecool/cookta/CooktaApplication.java
@@ -0,0 +1,14 @@
+package com.codecool.cookta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooktaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CooktaApplication.class, args);
+    }
+
+}
+
