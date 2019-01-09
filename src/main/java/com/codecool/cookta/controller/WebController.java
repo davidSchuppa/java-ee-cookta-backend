725318bed060df13ed6889404e930a994a0255f3
David Schuppa <d.schuppa@gmail.com>
2019-01-09 19:38:00 +0100

    refactor

--- src/main/java/com/codecool/cookta/controller/WebController.java
+++ src/main/java/com/codecool/cookta/controller/WebController.java
@@ -7,13 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class WebController {
-
-    List<Recipe> recipeList = new ArrayList<>();
 
     private final RequestHandler requestHandler;
     private final JsonMapper jsonMapper;
@@ -26,7 +23,6 @@ public class WebController {
 
     @GetMapping("/")
     public List<Recipe> listRecipe() {
-        System.out.println(jsonMapper.mapFilteredJson(requestHandler.fetchData("")));
         return jsonMapper.mapFilteredJson(requestHandler.fetchData(""));
 
     }
