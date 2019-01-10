a3f3f0474158bafe4c26d059d51e7693e2b29a0c
David Schuppa <d.schuppa@gmail.com>
2019-01-10 23:01:41 +0100

    search function now parametized

--- src/main/java/com/codecool/cookta/controller/WebController.java
+++ src/main/java/com/codecool/cookta/controller/WebController.java
@@ -24,20 +24,22 @@ public class WebController {
 
     @GetMapping("/api")
     public List<Recipe> listRecipe() {
-        return jsonMapper.mapFilteredJson(requestHandler.fetchData(""));
+        return jsonMapper.mapFilteredJson(requestHandler.fetchData("chicken", ""));
     }
 
 
     @RequestMapping("/api/search/")
-    public String searchByURL(HttpServletRequest request) {
-        System.out.println(request.getQueryString());
-        return "jeeeejjj";
+    public List<Recipe> searchByURL(HttpServletRequest request, @RequestParam String q) {
+        String params = request.getQueryString();
+        int startOfParams = params.indexOf('&');
+        String searchParams = params.substring(startOfParams);
+        return jsonMapper.mapFilteredJson(requestHandler.fetchData(q, searchParams));
     }
 
-    @PostMapping
-    (path = "/api/search/", consumes = "application/json", produces = "application/json")
-    public List<Recipe> search(@RequestBody String reqBody) {
-        System.out.println(reqBody);
-        return jsonMapper.mapFilteredJson(requestHandler.fetchData("?q=chicken&app_id=5b5897f7&app_key=9ac6d44f07118d8a2bead5a790b270d5&from=3&to=6&calories=591-722&health=alcohol-free"));
-    }
+//    @PostMapping
+//    (path = "/api/search/", consumes = "application/json", produces = "application/json")
+//    public List<Recipe> search(@RequestBody String reqBody) {
+//        System.out.println(reqBody);
+//        return jsonMapper.mapFilteredJson(requestHandler.fetchData("?q=chicken&app_id=5b5897f7&app_key=9ac6d44f07118d8a2bead5a790b270d5&from=3&to=6&calories=591-722&health=alcohol-free"));
+//    }
 }
