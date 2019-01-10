bb6115d760602599437dc41cf5f0ad3a1cff1b3d
David Szentesi <szntsd@gmail.com>
2019-01-10 13:04:06 +0100

    testing frontend

--- src/main/java/com/codecool/cookta/controller/WebController.java
+++ src/main/java/com/codecool/cookta/controller/WebController.java
@@ -4,6 +4,7 @@ import com.codecool.cookta.model.Recipe;
 import com.codecool.cookta.service.JsonMapper;
 import com.codecool.cookta.service.RequestHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class WebController {
         this.jsonMapper = jsonMapper;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/")
     public List<Recipe> listRecipe() {
         return jsonMapper.mapFilteredJson(requestHandler.fetchData(""));
