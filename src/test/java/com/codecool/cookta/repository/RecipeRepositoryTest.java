855cd31d43512977a73b2d500dd4477cb11935d1
adamszabolcs <szabolcs.adam91@gmail.com>
2019-01-22 17:07:23 +0100

    created recipe tests

--- src/test/java/com/codecool/cookta/repository/RecipeRepositoryTest.java
+++ src/test/java/com/codecool/cookta/repository/RecipeRepositoryTest.java
@@ -1,26 +1,89 @@
 package com.codecool.cookta.repository;
 
-import com.codecool.cookta.model.recipe.Recipe;
+import com.codecool.cookta.model.recipe.IngredientLines;
+import com.codecool.cookta.model.recipe.RecipeDb;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class RecipeRepositoryTest {
 
     @Autowired
     private RecipeRepository recipeRepository;
 
     @Test
+    @Transactional
     public void SaveRecipe() {
-        assertThat(true).isEqualTo(true);
+        IngredientLines ingredientLine = IngredientLines.builder()
+                .ingredient("first ingredient line")
+                .build();
+
+        RecipeDb recipe = RecipeDb.builder()
+                .image("image_path")
+                .label("chicken")
+                .ingredientLine(ingredientLine)
+                .url("url_for_description")
+                .build();
+
+        recipeRepository.save(recipe);
+
+        List<RecipeDb> recipeDbList = recipeRepository.findAll();
+
+        assertThat(recipeDbList)
+                .hasSize(1)
+                .containsOnly(recipe);
     }
 
+    @Test(expected = DataIntegrityViolationException.class)
+    public void cantSaveWithoutLabel() {
+        IngredientLines line = IngredientLines.builder()
+                .ingredient("first ingredient line")
+                .build();
+
+        RecipeDb recipe = RecipeDb.builder()
+                .image("image_path")
+                .ingredientLine(line)
+                .url("url_for_description")
+                .build();
+
+        recipeRepository.save(recipe);
+    }
+
+    @Test(expected = DataIntegrityViolationException.class)
+    public void cantSaveWithoutIngredients() {
+        RecipeDb recipe = RecipeDb.builder()
+                .label("label")
+                .url("url_for_desc")
+                .build();
+
+        recipeRepository.save(recipe);
+    }
+
+    @Test(expected = DataIntegrityViolationException.class)
+    public void cantSaveWithoutDescription() {
+        IngredientLines line = IngredientLines.builder()
+                .ingredient("first ingredient line")
+                .build();
+
+        RecipeDb recipe = RecipeDb.builder()
+                .label("label")
+                .ingredientLine(line)
+                .build();
+
+        recipeRepository.save(recipe);
+    }
 }
