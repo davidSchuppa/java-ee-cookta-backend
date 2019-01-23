e472ed26be8342f7f604bc5df1185126c9bf0a86
adamszabolcs <szabolcs.adam91@gmail.com>
2019-01-23 15:02:08 +0100

    started implementing registerUser method

--- src/main/java/com/codecool/cookta/service/RegisterUserService.java
+++ src/main/java/com/codecool/cookta/service/RegisterUserService.java
@@ -1,4 +1,19 @@
 package com.codecool.cookta.service;
 
+import com.codecool.cookta.model.CooktaUser;
+import com.codecool.cookta.repository.CooktaUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RegisterUserService {
+
+    @Autowired
+    private CooktaUserRepository cooktaUserRepository;
+
+
+    public void registerUser(CooktaUser cooktaUser) {
+        cooktaUserRepository.save(cooktaUser);
+    }
+
 }
