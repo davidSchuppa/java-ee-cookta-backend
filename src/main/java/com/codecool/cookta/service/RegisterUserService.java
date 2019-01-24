80d2e56161b94a7ce0d47340fb8a57a3836811f0
adamszabolcs <szabolcs.adam91@gmail.com>
2019-01-24 10:25:27 +0100

    on register, user gets a diet and a health, encoded password saved to db

--- src/main/java/com/codecool/cookta/service/RegisterUserService.java
+++ src/main/java/com/codecool/cookta/service/RegisterUserService.java
@@ -1,8 +1,11 @@
 package com.codecool.cookta.service;
 
 import com.codecool.cookta.model.CooktaUser;
+import com.codecool.cookta.model.intolerance.Diet;
+import com.codecool.cookta.model.intolerance.Health;
 import com.codecool.cookta.repository.CooktaUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,11 +14,18 @@ public class RegisterUserService {
     @Autowired
     private CooktaUserRepository cooktaUserRepository;
 
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
 
     public boolean registerUser(CooktaUser cooktaUser) {
         if (!cooktaUserRepository.existsCooktaUserByUsername(cooktaUser.getUsername())
                 &&
                 !cooktaUserRepository.existsCooktaUserByEmail(cooktaUser.getEmail())) {
+            cooktaUser.setPassword(passwordEncoder.encode(cooktaUser.getPassword()));
+            Diet defaultDiet = Diet.builder().build();
+            Health defaultHealth = Health.builder().build();
+            cooktaUser.setDiet(defaultDiet);
+            cooktaUser.setHealth(defaultHealth);
             cooktaUserRepository.save(cooktaUser);
             return true;
         }
