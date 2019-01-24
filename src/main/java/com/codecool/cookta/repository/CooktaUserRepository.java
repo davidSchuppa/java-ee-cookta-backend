ddd5475b8db514f518282f3109859febd2adbc42
adamszabolcs <szabolcs.adam91@gmail.com>
2019-01-24 10:21:44 +0100

    unnecessary imports deleted

--- src/main/java/com/codecool/cookta/repository/CooktaUserRepository.java
+++ src/main/java/com/codecool/cookta/repository/CooktaUserRepository.java
@@ -2,8 +2,6 @@ package com.codecool.cookta.repository;
 
 import com.codecool.cookta.model.CooktaUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface CooktaUserRepository extends JpaRepository<CooktaUser ,Long> {
 
