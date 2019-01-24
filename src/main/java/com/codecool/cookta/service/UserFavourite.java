6eb8812e3bf0a0de1778aee52b345a75712e7849
adamszabolcs <szabolcs.adam91@gmail.com>
2019-01-24 11:35:16 +0100

    save favourite recipe for user

---
+++ src/main/java/com/codecool/cookta/service/UserFavourite.java
@@ -0,0 +1,29 @@
+package com.codecool.cookta.service;
+
+import com.codecool.cookta.model.CooktaUser;
+import com.codecool.cookta.model.recipe.RecipeDb;
+import com.codecool.cookta.repository.CooktaUserRepository;
+import com.codecool.cookta.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserFavourite {
+
+    @Autowired
+    private CooktaUserRepository cooktaUserRepository;
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    public void addFavourite(String username, RecipeDb recipe) {
+        CooktaUser cooktaUserByUsername = cooktaUserRepository.findCooktaUserByUsername(username);
+        if (recipeRepository.existsRecipeDbByUrl(recipe.getUrl())) {
+            RecipeDb newRecipe = recipeRepository.findRecipeDbByUrl(recipe.getUrl());
+        } else {
+            recipeRepository.save(recipe);
+        }
+        cooktaUserByUsername.appendFavourite(recipe);
+    }
+
+}
