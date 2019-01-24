c13da3bb1ab7e040e965d22784877b109c890b4f
adamszabolcs <szabolcs.adam91@gmail.com>
2019-01-24 10:24:29 +0100

    BCryptPasswordEncoder added, used for login validation

--- src/main/java/com/codecool/cookta/service/LoginValidation.java
+++ src/main/java/com/codecool/cookta/service/LoginValidation.java
@@ -3,6 +3,7 @@ package com.codecool.cookta.service;
 import com.codecool.cookta.model.CooktaUser;
 import com.codecool.cookta.repository.CooktaUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -14,11 +15,13 @@ public class LoginValidation {
     @Autowired
     private CooktaUserRepository cooktaUserRepository;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     public LoginData validation(Map<String, String> data) throws IllegalAccessException {
         try {
             CooktaUser user = cooktaUserRepository.findCooktaUserByUsername(data.get("username"));
             LoginData loginData = new LoginData(user.getId(), user.getUsername(), user.getDiet().getDietFields(), user.getHealth().getHealthFields());
-            if (user.getPassword().equals(data.get("password"))) {
+            if (encoder.matches(data.get("password"), user.getPassword())) {
                 return loginData;
             } else {
                 return null;
