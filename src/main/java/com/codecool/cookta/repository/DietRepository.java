23317d259c8a3b05adfc9e1d285e2edaf70888b2
Gábor Kovács <gabor950601@gmail.com>
2019-02-05 10:32:52 +0100

    clean code

--- src/main/java/com/codecool/cookta/repository/DietRepository.java
+++ src/main/java/com/codecool/cookta/repository/DietRepository.java
@@ -3,7 +3,10 @@ package com.codecool.cookta.repository;
 import com.codecool.cookta.model.intolerance.Diet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Map;
+
 public interface DietRepository extends JpaRepository<Diet, Long> {
 
     Diet findDietByCooktaUserId(Long id);
+
 }
