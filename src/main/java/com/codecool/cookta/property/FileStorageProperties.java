6b6f805201f3508e603ee9c07ddceac6194a5e4b
adamszabolcs <szabolcs.adam91@gmail.com>
2019-02-06 11:16:02 +0100

    created class, started implementing

--- src/main/java/com/codecool/cookta/property/FileStorageProperties.java
+++ src/main/java/com/codecool/cookta/property/FileStorageProperties.java
@@ -1,4 +1,12 @@
 package com.codecool.cookta.property;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "file")
 public class FileStorageProperties {
+
+    private String uploadDir;
+
 }
