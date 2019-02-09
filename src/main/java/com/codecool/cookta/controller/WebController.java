6fd58f51d96c14074a1ca6dda757aa498fc42a8a
adamszabolcs <szabolcs.adam91@gmail.com>
2019-02-09 14:11:03 +0100

    regrouped uploadRecipe method to service call

--- src/main/java/com/codecool/cookta/controller/WebController.java
+++ src/main/java/com/codecool/cookta/controller/WebController.java
@@ -1,11 +1,9 @@
 package com.codecool.cookta.controller;
 
 import com.codecool.cookta.model.CooktaUser;
+import com.codecool.cookta.model.LoginData;
 import com.codecool.cookta.model.dto.Recipe;
-import com.codecool.cookta.model.intolerance.Diet;
-import com.codecool.cookta.model.intolerance.Health;
 import com.codecool.cookta.model.recipe.RecipeDb;
-import com.codecool.cookta.payload.UploadFileResponse;
 import com.codecool.cookta.repository.CooktaUserRepository;
 import com.codecool.cookta.repository.RecipeRepository;
 import com.codecool.cookta.service.*;
@@ -16,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -29,27 +26,27 @@ public class WebController {
 
     private final EdamamAPIService requestHandler;
     private final JsonMapper jsonMapper;
-    private final RegisterUserService registerUserService;
     private final LoginValidation loginValidation;
     private final UserFavourite userFavourite;
     private final CooktaUserRepository cooktaUserRepository;
-    private final UserIntolerance userIntolerance;
-    private final FileStorageService fileStorageService;
-    private final RecipeRepository recipeRepository;
-    private final RecipeIntolerance recipeIntolerance;
+    private final IntoleranceSetterService intoleranceSetterService;
+    private final RecipeUploadService recipeUploadService;
 
     @Autowired
-    public WebController(EdamamAPIService requestHandler, JsonMapper jsonMapper, RegisterUserService registerUserService, LoginValidation loginValidation, UserFavourite userFavourite, CooktaUserRepository cooktaUserRepository, UserIntolerance userIntolerance, FileStorageService fileStorageService, RecipeRepository recipeRepository, RecipeIntolerance recipeIntolerance) {
+    public WebController(EdamamAPIService requestHandler,
+                         JsonMapper jsonMapper,
+                         LoginValidation loginValidation,
+                         UserFavourite userFavourite,
+                         CooktaUserRepository cooktaUserRepository,
+                         IntoleranceSetterService intoleranceSetterService,
+                         RecipeUploadService recipeUploadService) {
         this.requestHandler = requestHandler;
         this.jsonMapper = jsonMapper;
-        this.registerUserService = registerUserService;
         this.loginValidation = loginValidation;
         this.userFavourite = userFavourite;
         this.cooktaUserRepository = cooktaUserRepository;
-        this.userIntolerance = userIntolerance;
-        this.fileStorageService = fileStorageService;
-        this.recipeRepository = recipeRepository;
-        this.recipeIntolerance = recipeIntolerance;
+        this.intoleranceSetterService = intoleranceSetterService;
+        this.recipeUploadService = recipeUploadService;
     }
 
     @RequestMapping("/api")
@@ -118,62 +115,15 @@ public class WebController {
 
     @RequestMapping(value = "/api/intolerance/{username}", headers = "Accept=application/json")
     public ResponseEntity<?> saveUserIntolerance(@PathVariable("username") String username, @RequestBody Map<String, Map<String, Boolean>> data) throws IllegalAccessException {
-        userIntolerance.updateIntolerance(username, data);
+        intoleranceSetterService.updateUserIntolerance(username, data);
         log.debug(username);
         log.debug(Arrays.asList(data).toString());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping(value = "/api/recipe")
-    public ResponseEntity<?> uploadRecipe(@RequestBody String data)
-            throws IllegalAccessException {
-        log.debug(data);
-        ObjectMapper mapper = new ObjectMapper();
-        RecipeDb recipe = null;
-        Map<String, Boolean> dietMap = new HashMap<>();
-        Map<String, Boolean> healthMap = new HashMap<>();
-        try {
-            JsonNode dataTree = mapper.readTree(data);
-            String label = mapper.treeToValue(dataTree.get("label"), String.class);
-            String ingredientLines = mapper.treeToValue(dataTree.get("ingredientLines"), String.class);
-            List<String> diet = mapper.treeToValue(dataTree.get("diet"), List.class);
-            List<String> health = mapper.treeToValue(dataTree.get("health"), List.class);
-            String fileName = mapper.treeToValue(dataTree.get("filename"), String.class);
-
-            for (String elem : diet) {
-                dietMap.put(elem, true);
-            }
-            for (String elem : health) {
-                healthMap.put(elem, true);
-            }
-
-            String image = "http://localhost:8080/downloadFile/" + fileName;
-            recipe = RecipeDb.builder().label(label).ingredientLine(ingredientLines).url(fileName).image(image).build();
-
-            Diet defaultDiet = Diet.builder().build();
-            Health defaultHealth = Health.builder().build();
-
-            recipe.setRecipeDiet(defaultDiet);
-            recipe.setRecipeHealth(defaultHealth);
-
-            recipeRepository.save(recipe);
-            recipeIntolerance.updateRecipeHealthIntolerance(label, healthMap);
-            recipeIntolerance.updateRecipeDietIntolerance(label, dietMap);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-//
-//        recipeRepository.save(recipe);
-//        String url = "http://localhost:8080/api/userRecipe/";
-//        System.out.println("itt mentettem el");
-//        recipe.setUrl(url);
-//        recipeRepository.save(recipe);
-//        recipeIntolerance.updateRecipeIntolerance(url, healthLabel);
-//        recipeIntolerance.updateRecipeIntolerance(url, dietLabel);
+    public ResponseEntity<?> uploadRecipe(@RequestBody String data) throws IllegalAccessException {
+        recipeUploadService.uploadRecipe(data);
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
-
 }
